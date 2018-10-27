af7a6b1831086e99afd17570bed146a67c7e286d
Tak H Kim <takhyun818@gmail.com>
2018-10-27 21:34:19 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-
+  BRAND = []
 
   def initialize(brand)
     @brand = brand
   end
-
 
   def cobble
     self.condition = "new"
